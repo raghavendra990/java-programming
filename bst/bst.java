47bda422cb069a4736285b5a7080a516022f58dc
Raghavendra Yadav <raghavyaadav990@gmail.com>
2017-03-07 18:15:07 +0530

    delete a element from BST

--- bst/bst.java
+++ bst/bst.java
@@ -62,13 +62,63 @@ class BinarySearchTree{
 		}
 	}
 
+	// function to delete a perticular key from BST
+	void deleteKey(int key){
+		root = deleteRec(root, key);
+	}
+
+	// recursive fumction to insert the key in BST
+	Node deleteRec(Node root, int key){
+		//  Base Case: id the tree is empty 
+		if (root == null) return root;
+
+		// otherwise reoccur down to tree
+		if (key<root.key){
+			root.left = deleteRec(root.left, key);
+		}
+		else if (key>root.key){
+			root.right = deleteRec(root.right, key);
+		}
+
+		// if the is same as root key
+		else{
+
+			// node with only one child
+			if (root.left == null){
+				return root.right;
+			}
+			else if (root.right == null){
+				return root.left;
+			}
+
+			// if node has both the children
+			// than get inorder succesor of the sub tree (smallest is thee right sub tree)
+
+			root.key = minValue(root.right);
+
+			root.right = deleteRec(root.right,  root.key); 
+		}
+		return root;
+	}
+
+	// function to find minimum value in the tree
+	int minValue(Node root){
+		int minv = root.key;
+
+		while(root.left != null){
+			minv = root.left.key;
+			root = root.left;
+		}
+		return minv;
+	}
+
 	// programm to test the above function
 	public static void main(String[] args) {
 		BinarySearchTree tree = new BinarySearchTree();
 		System.out.println(tree.root);
 	
 	// tree.root;
-	tree.insert(50);
+		tree.insert(50);
         tree.insert(30);
         tree.insert(20);
         tree.insert(40);
@@ -77,6 +127,9 @@ class BinarySearchTree{
         tree.insert(80);
  
         // print inorder traversal of the BST
+        tree.inorder();
+        tree.deleteKey(340);
+        System.out.println("\n");
         tree.inorder();
 }
 	}
